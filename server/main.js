88a089f6b9a644baafd4c7cd837c92b8535cac33
Mikołaj <22h39@tutanota.com>
2020-11-23 20:43:57 +0000

    TPS

--- server/main.js
+++ server/main.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 80
+var fs = require('fs')
+var http = require('http')
+var https = require('https')
+var privateKey  = fs.readFileSync('/opt/keys/privkey.pem', 'utf8')
+var certificate = fs.readFileSync('/opt/keys/fullchain.pem', 'utf8')
 
-app.get('/', (req, res) => {
-	res.send('Dupa')
-})
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
 
-app.listen(port, () => {
-	console.log('Recieved dup dupa')
-})
+app.use(express.static('static'))
+
+app.get('/wyborinsko', function(req,res) {
+    res.send('hello');
+});
+
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80);
+httpsServer.listen(443);
